ebf1157e749f6861244d62adf8e413855d223aed
Joel Martin <joel@Joels-MacBook-Pro.local>
2025-07-02 15:42:05 +0100

    File name change

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 export * from './globals.js';
-export * from './home_page.js';
+export * from './general.js';
 export * from './project.js';
 export * from './quiz.js';
 export * from './hero_slider.js';
